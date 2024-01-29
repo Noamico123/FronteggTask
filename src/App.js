c660b7d75e2171bc46723fe777aa1a4b76593940
Noamico123 <noamico123@gmail.com>
2024-01-29 10:41:45 +0200

    finished tenant button switch

--- src/App.js
+++ src/App.js
@@ -1,34 +1,40 @@
 import './App.css';
 import { useEffect } from 'react';
-import { useAuth, useLoginWithRedirect, ContextHolder, AdminPortal } from "@frontegg/react";
+import { useAuth, useLoginWithRedirect, ContextHolder, AdminPortal, useAuthActions } from "@frontegg/react";
 
 function App() {
   const { user, isAuthenticated } = useAuth();
   const loginWithRedirect = useLoginWithRedirect();
+  const { switchTenant } = useAuthActions();
 
-  // Uncomment this to redirect to login automatically
+  //redirect when the user is not loggedin
   useEffect(() => {
     if (!isAuthenticated) {
-  loginWithRedirect();
-    }
+    loginWithRedirect();
+      }
   }, [isAuthenticated, loginWithRedirect]);
-
+  
+  //Divider line (design)
   const Divider = () => {
     return (
-        <hr
-            style={{ borderBottom: "1px dotted lightgrey",width: "100%"}}
-        ></hr>
+        <hr style={{ borderBottom: "1px dotted lightgrey",width: "100%"}}/>
     );
   };
-  
 
-  const logout = () => {
+  //logout button
+  const handleLogout = () => {
     const baseUrl = ContextHolder.getContext().baseUrl;
     window.location.href = `${baseUrl}/oauth/logout?post_logout_redirect_uri=${window.location}`;
   };
 
-  const handleClick = () => {
+  //settings button
+  const handleAdminPortalClick = () => {
     AdminPortal.show()
+  }
+
+  // switch user button
+  const handleSwitchTenant = (tenantId) => {
+    switchTenant({ tenantId });
   }
 
   return (
@@ -40,18 +46,24 @@ function App() {
             <span className='card-name'>Hi {user?.name}</span>
           </div>
           <Divider />
+          <select className='button' value={user?.tenantId} onChange={e => handleSwitchTenant(e.target.value)}>
+            <option disabled>Select Tenant</option>
+            {user.tenantIds.map(tenantId => (
+              <option key={tenantId} value={tenantId}>
+                {tenantId}
+              </option>
+            ))}
+          </select>
+
           <div>
             <button className='button' onClick={() => alert(user.accessToken)}>View Token</button>
           </div>
           <div>
-            <button className='button' onClick={() => handleClick()}>Settings</button>
+            <button className='button' onClick={() => handleAdminPortalClick()}>Settings</button>
           </div>
           <div>
-            <button className='button' onClick={() => logout()}>Logout</button>
+            <button className='button' onClick={() => handleLogout()}>Logout</button>
           </div>
-          
-
-          
         </div>
       ) : (
         <div>
